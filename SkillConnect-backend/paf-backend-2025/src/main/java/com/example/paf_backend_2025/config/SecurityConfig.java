a66e33d3faf70ae2367efe135e19bf297e0b4551
BuddhikaRoshan <buddhikaroshanofficial@gmail.com>
2025-04-26 23:02:05 +0530

    "Update Package name"

--- SkillConnect-backend/paf-backend-2025/src/main/java/com/example/paf_backend_2025/config/SecurityConfig.java
+++ SkillConnect-backend/paf-backend-2025/src/main/java/com/example/paf_backend_2025/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.pafbackend.config;
+package com.example.paf_backend_2025.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
