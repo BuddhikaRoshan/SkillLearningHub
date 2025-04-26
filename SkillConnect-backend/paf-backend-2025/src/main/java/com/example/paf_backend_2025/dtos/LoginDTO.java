277584fedded0cdb68bab75e514cae814341c9a2
BuddhikaRoshan <buddhikaroshanofficial@gmail.com>
2025-04-26 21:39:10 +0530

    "add user management codes"

--- SkillConnect-backend/paf-backend-2025/src/main/java/com/example/paf_backend_2025/dtos/LoginDTO.java
+++ SkillConnect-backend/paf-backend-2025/src/main/java/com/example/paf_backend_2025/dtos/LoginDTO.java
@@ -1,5 +1,23 @@
 package com.example.paf_backend_2025.dtos;
 
 public class LoginDTO {
-    
+    private String username;
+    private String password;
+
+    // Getters and Setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- SkillConnect-backend/paf-backend-2025/src/main/java/com/example/paf_backend_2025/dtos/PublicStatusRequest.java
+++ SkillConnect-backend/paf-backend-2025/src/main/java/com/example/paf_backend_2025/dtos/PublicStatusRequest.java
@@ -1,5 +1,13 @@
 package com.example.paf_backend_2025.dtos;
 
 public class PublicStatusRequest {
-    
+    private boolean isPublic;
+
+    public boolean isPublic() {
+        return isPublic;
+    }
+
+    public void setPublic(boolean aPublic) {
+        this.isPublic = aPublic;
+    }
 }
