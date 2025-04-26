c773cc5e688d9021c32256e696514c464ba3981c
BuddhikaRoshan <buddhikaroshanofficial@gmail.com>
2025-04-26 21:50:29 +0530

    Create AppUserRepository.java

---
+++ SkillConnect-backend/paf-backend-2025/src/main/java/com/example/paf_backend_2025/repositories/AppUserRepository.java
@@ -0,0 +1,15 @@
+package com.example.paf_backend_2025.repositories;
+
+import com.example.paf_backend_2025.models.AppUser;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AppUserRepository extends MongoRepository<AppUser, String> {
+    Optional<AppUser> findByUsername(String username);
+    Optional<AppUser> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+}
