8b78d09e71c17fa52ec0be9548e50a96628e36aa
BuddhikaRoshan <buddhikaroshanofficial@gmail.com>
2025-04-27 12:39:16 +0530

    "update package"

--- SkillConnect-backend/paf-backend-2025/src/main/java/com/example/paf_backend_2025/repositories/NotificationRepository.java
+++ SkillConnect-backend/paf-backend-2025/src/main/java/com/example/paf_backend_2025/repositories/NotificationRepository.java
@@ -1,7 +1,7 @@
-package com.example.pafbackend.repositories;
+package com.example.paf_backend_2025.repositories;
 
-import com.example.pafbackend.models.AppUser;
-import com.example.pafbackend.models.Notification;
+import com.example.paf_backend_2025.models.AppUser;
+import com.example.paf_backend_2025.models.Notification;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
